4659ffcb8fdaf7fd9658c2d9bd3483d9a5238224
Roman Kutselepa <roman.kutselepa@gmail.com>
2025-05-23 13:09:10 +0300

    JS homework

---
+++ Lesson-6-js/homework.js
@@ -0,0 +1,30 @@
+// a. Convert the entire string to uppercase.
+// b. Replace "Alex" with your name.
+// c. Count how many characters are in the new string.
+// d. Check if the string includes the word "name".
+let greeting = "Hello, my name is Alex.";
+
+
+
+// a. Add them together.
+// b. Multiply them.
+// c. Convert to string, leaving two digits after coma
+let num1 = 8;
+let num2 = 3.5;
+
+
+
+
+// a. Create a string that says:
+// The product "Basketball" costs $24.90.
+// (but using the variables, not writing the sentence manually!)
+// b. Make entire sentence uppercase
+
+let product = "Basketball";
+let price = 24.9;
+
+
+
+// Write a line of code that takes this string:
+// and trims the whitespace, then shows the final length of the trimmed string.
+let message = "   JavaScript is fun!   ";
